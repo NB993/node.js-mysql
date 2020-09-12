a7e4df55f40b2b966f8fd3316c2c0bdc053fbbe0
jhchoe <qnalsfj@gmail.com>
2020-09-13 02:04:42 +0900

    5강. MySQL join을 이용해서 상세보기 구현까지

--- nodejs/mysql.js
+++ nodejs/mysql.js
@@ -1,15 +1,15 @@
 var mysql      = require('mysql');
 // 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다. 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
+  host     : 'localhost', //서버위치. node.js와 mysql의 서버가 같은 컴퓨터에 있을 땐 localhost
+  user     : 'root', //db계정
   password : '111111',
-  database : 'opentutorials'
+  database : 'opentutorials' //mysql> use <database name>과 같은 역할
 });
   
-connection.connect();
+connection.connect(); //접속
   
-connection.query('SELECT * FROM topic', function (error, results, fields) {
+connection.query('SELECT * FROM topic', function (error, results, fields) { //
     if (error) {
         console.log(error);
     }
